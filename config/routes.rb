3c2cfe51e83ea17cd360740d683d460b688b2b4c
Serlle Rosales <jesus.rosles@concretosdcc.com>
2021-05-25 11:55:49 -0500

    Estoy en la parte de rutas. Realice una practica con los params

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+
   get '/books', to: 'books#index'
-  root 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/pages/:name', to: 'pages#home'
+
 end
